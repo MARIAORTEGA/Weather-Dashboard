20964f33b86817a79410f636f7680c4e5063a208
Maria Ortega <mariaortegapb@gmail.com>
2020-11-28 11:27:31 -0500

    added AJAX

---
+++ script.js
@@ -0,0 +1,49 @@
+// GIVEN a weather dashboard with form inputs WHEN I search for a city  
+// THEN I am presented with current and future conditions for that city and that city is added to the search history  
+
+
+// WHEN I view current weather conditions for that city  
+// THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index 
+
+
+// WHEN I view the UV index  
+// THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe  
+
+
+// WHEN I view future weather conditions for that city  
+// THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, and the humidity  
+
+ 
+// WHEN I click on a city in the search history  
+// THEN I am again presented with current and future conditions for that city  
+
+ 
+// WHEN I open the weather dashboard  
+// THEN I am presented with the last searched city forecast 
+
+
+
+//JSON content in the div 
+Function displayCity() { 
+
+    var city = $(this).attr(“city-data”); 
+    var queryURL = “https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=e7b75a6005a289d2d814dd84709cf083";
+    
+
+$.ajax({ 
+    url: queryURL, 
+    method: “GET” 
+}).then(function(response){ 
+    $(“#See-City”).text(JSON.stringify(response)); 
+}); 
+} 
+
+// for showing city data 
+renderButtons(); 
+}); 
+
+
+//function for showing city info 
+$(document)on(“click”, “city”, displayCify); 
+
+//my API key e7b75a6005a289d2d814dd84709cf083
