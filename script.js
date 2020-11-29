6c8467b0f645e81b035ac07002f7eb478f30d527
Maria Ortega <mariaortegapb@gmail.com>
2020-11-29 17:13:36 -0500

    added containers and rowns

--- script.js
+++ script.js
@@ -21,29 +21,53 @@
 // WHEN I open the weather dashboard  
 // THEN I am presented with the last searched city forecast 
 
+            //.on("click") function will start the AJAX call
+            $("#search-city").on("click", function(event) {
+                //option to hit enter instead of clicking button
+                event.preventDefault();
 
+                //get text from input box
+                var city = $("#city-input").val();
 
-//JSON content in the div 
-Function displayCity() { 
+                //make URL
+                var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=e7b75a6005a289d2d814dd84709cf083";
+                
+                //run AJAX call to API
+                $.ajax({ 
+                url: queryURL, 
+                method: "GET" 
+                }).then(function(response) {
+                $("#city-data").text(JSON.stringify(response)); 
+                $("#name").text(response.name)
+                //log queryURL and resulting object
+                console.log(queryURL);
+                console.log(response);
+                
+                
+            //div to hold city weather
+            // var cityDiv = $("<div class='city'>");
+            // //store city name
+            // var name = response.city.name;
+            // //display city name
+            // var nAme = $("<p>").text("City Name: " + name);
+            // //display city name
+            // cityDiv.append(nAme);
+                                                                                
+            //     console.log(nAme);
 
-    var city = $(this).attr(“city-data”); 
-    var queryURL = “https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=e7b75a6005a289d2d814dd84709cf083";
+        //making html to hold city info
+            // var cityName = $("<h2>").text(response.name);  
+            // var weatherIcon = $("<icon>").html("img src=",response.icon);
+            // var date = $( )
+        
+            // //var date = $(.datepicker.formatDate('yy/mm/dd', new Date());
+            // //var date = $(mm + '/' + dd + '/' + yyyy);
     
 
-$.ajax({ 
-    url: queryURL, 
-    method: “GET” 
-}).then(function(response){ 
-    $(“#See-City”).text(JSON.stringify(response)); 
-}); 
-} 
+            // $("#city-data").empty();
+            // $("#city-data").append(cityName, date, weatherIcon);
 
-// for showing city data 
-renderButtons(); 
-}); 
+            // console.log(date);
 
-
-//function for showing city info 
-$(document)on(“click”, “city”, displayCify); 
-
-//my API key e7b75a6005a289d2d814dd84709cf083
+        });
+               });
